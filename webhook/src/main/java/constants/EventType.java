85a9261adc80b76d11ab75c9dbeaee39efe9c985
florenwu <florenwu@tencent.com>
2022-06-10 09:52:56 +0800

    feat:add EventType

---
+++ webhook/src/main/java/constants/EventType.java
@@ -0,0 +1,17 @@
+package constants;
+
+public enum EventType {
+
+    /**
+     * publish config
+     */
+    PUBLISH,
+    /**
+     * edit config
+     */
+    EDIT,
+    /**
+     * delete config
+     */
+    DELETE,
+}
